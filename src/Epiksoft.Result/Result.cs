b54fe0b5a6e6b12952d947b7603016d0cb91abee
Mehmet Kekeç <mehmet552ke@gmail.com>
2023-03-15 23:29:26 +0300

    feat: added basic result properties

---
+++ src/Epiksoft.Result/Result.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+
+namespace Epiksoft.Result;
+
+public class Result
+{
+	public int Status { get; }
+	public string Message { get; }
+	public string Code { get; }
+	public bool Succeeded { get; }
+	public ICollection<ResultError> Errors { get; set; }
+	public HttpStatusCode HttpStatusCode { get; }
+
+	public Result(int status, string message, string code, HttpStatusCode? httpStatusCode = null)
+	{
+		Status = status;
+		Message = message;
+		Code = code;
+
+		HttpStatusCode = httpStatusCode ?? (status is 1 ? HttpStatusCode.OK : HttpStatusCode.BadRequest);
+		Succeeded = status is 1;
+	}
+
+	public static Result Success(string message, string code, HttpStatusCode? httpStatusCode = null) => new(1, message, code, httpStatusCode);
+
+	public static Result Success(HttpStatusCode? httpStatusCode = null) => new(1, string.Empty, string.Empty, httpStatusCode);
+
+	public static Result<TData> Success<TData>(TData data, string message, string code, HttpStatusCode? httpStatusCode = null) => new(data, 1, message, code, httpStatusCode);
+
+	public static Result<TData> Success<TData>(TData data, HttpStatusCode? httpStatusCode = null) => new(data, 1, string.Empty, string.Empty, httpStatusCode);
+
+	public static Result Failure(string message, string code, HttpStatusCode? httpStatusCode = null) => new(0, message, code, httpStatusCode);
+
+	public static Result Failure(HttpStatusCode? httpStatusCode = null) => new(0, string.Empty, string.Empty, httpStatusCode);
+
+	public static Result<TData> Failure<TData>(TData data, string message, string code, HttpStatusCode? httpStatusCode = null) => new(data, 0, message, code, httpStatusCode);
+
+	public static Result<TData> Failure<TData>(TData data, HttpStatusCode? httpStatusCode = null) => new(data, 1, string.Empty, string.Empty, httpStatusCode);
+
+}
+
+public class Result<TData> : Result
+{
+	public TData Data { get; }
+
+	public Result(TData data, int status, string message, string code, HttpStatusCode? httpStatusCode = null) : base(status, message, code, httpStatusCode)
+	{
+		Data = data;
+	}
+
+}
+
+
+
--- src/Epiksoft.Result/ResultError.cs
+++ src/Epiksoft.Result/ResultError.cs
@@ -1,25 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Epiksoft.Result;
 
-namespace Epiksoft.Result
+public sealed class ResultError
 {
-    public sealed class ResultError
-    {
-        public string Message { get; }
-        public string Code { get; }
+	public string Message { get; }
+	public string Code { get; }
 
-        /// <summary>
-        /// constructor to create result error
-        /// </summary>
-        /// <param name="message">your message  e.g. "email already in use"</param>
-        /// <param name="code">your message code e.g. "email_already_in_use"</param>
-        public ResultError(string message, string code)
-        {
-            Message = message;
-            Code = code;
-        }
-    }
+	/// <summary>
+	/// constructor to create result error
+	/// </summary>
+	/// <param name="message">your message  e.g. "email already in use"</param>
+	/// <param name="code">your message code e.g. "email_already_in_use"</param>
+	public ResultError(string message, string code)
+	{
+		Message = message;
+		Code = code;
+	}
 }

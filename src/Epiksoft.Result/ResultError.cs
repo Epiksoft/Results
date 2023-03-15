90bf637491b4cf1bd23104c0a83dda31768cdfcc
Emre Özgenç <emreozgenc@hotmail.com.tr>
2023-03-15 22:34:57 +0300

    feat: added result error class

---
+++ src/Epiksoft.Result/ResultError.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epiksoft.Result
+{
+    public sealed class ResultError
+    {
+        public string Message { get; }
+        public string Code { get; }
+
+        /// <summary>
+        /// constructor to create result error
+        /// </summary>
+        /// <param name="message">your message  e.g. "email already in use"</param>
+        /// <param name="code">your message code e.g. "email_already_in_use"</param>
+        public ResultError(string message, string code)
+        {
+            Message = message;
+            Code = code;
+        }
+    }
+}
